c9564de6dbe1e89b00b6798e8267d6dfcdf70e1b
misha <mabotov2002@gmail.com>
2024-10-16 18:13:20 +0300

    правки

--- src/test/java/com/example/backend_minor/BackendMinorApplicationTests.java
+++ src/test/java/com/example/backend_minor/BackendMinorApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.backend_minor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BackendMinorApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(5, 5);
     }
 
 }
